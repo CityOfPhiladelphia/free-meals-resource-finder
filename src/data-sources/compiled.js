22b6cfaf86fcf15eb442a0085c36bcc81380b81e
Andy Rothwell <rothwell.andy@gmail.com>
2020-06-10 14:55:47 -0400

    fixes issue with address search

--- src/data-sources/compiled.js
+++ src/data-sources/compiled.js
@@ -2,7 +2,7 @@ export default {
   id: 'compiled',
   // type: 'http-get',
   // dependent: 'none',
-  // resettable: false,
+  resettable: false,
   // url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Senior_Sites_PUBLIC_VIEW/FeatureServer/0/query',
   // options: {
   //   params: {
