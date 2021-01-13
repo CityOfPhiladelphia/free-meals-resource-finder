c9e62979423ae7d59ad4d4c552a4dbc39dcd4171
Karissa <k@karissademi.com>
2021-01-13 11:02:46 -0500

    Update for prod

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   filenameHashing: false,
-  // publicPath: "/food/",
-  publicPath: '/free-meals/dev/',
+  publicPath: "/food/",
+  //publicPath: '/free-meals/dev/',
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
   },
