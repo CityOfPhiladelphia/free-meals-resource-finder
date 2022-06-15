1780c6bb4efbb27d0fc11333b91522f2b4623b19
DESKTOP-DCLTBK8\rothw <rothwell.andy@gmail.com>
2022-06-15 11:13:11 -0400

    uses pinboard commit off of 1.1.1

--- vue.config.js
+++ vue.config.js
@@ -4,23 +4,6 @@ module.exports = {
     config.resolve.symlinks(false);
   },
   lintOnSave: true,
-
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       data: '@import "@/scss/global.scss;',
-  //     },
-  //   },
-  // },
-
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       prependData: `@import "@/scss/_variables.scss";
-  //             @import "@/scss/_mixins.scss";`,
-  //     },
-  //   },
-  // },
   css: {
     loaderOptions: {
       sass: {
@@ -29,31 +12,21 @@ module.exports = {
           @import "~@phila/phila-ui/src/assets/styles/scss/functions.scss";
           @import "~@phila/phila-ui/src/assets/styles/scss/colors.scss";
         `,
-        // @import "@/scss/_variables.scss";
-        // @import "@/scss/_mixins.scss";
+        sourceMap: true,
+        sassOptions: {
+          outputStyle: "compressed",
+        },
       },
     },
   },
 
-  // pluginOptions: {
-  // //   'style-resources-loader': {
-  // //     preProcessor: 'scss',
-  // //     patterns: [
-  // //       path.resolve(__dirname, '@/styles/global.scss'),
-  // //     ],
-  // //   },
-  // // },
-  // },
-  // assetsDir: 'static',
   transpileDependencies: [
     // can be string or regex
     '@phila/pinboard',
     '@phila/phila-ui',
-    // '@phila/vue-comps',
     '@phila/vue-mapping',
     '@phila/vue-datafetch',
     'fuse.js',
-    // /other-dep/
   ],
 
 };
