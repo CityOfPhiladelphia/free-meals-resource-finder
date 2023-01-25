1283d44711aca345b182ef29559a5b8882317aa2
m-atia <mohamed.atia@phila.gov>
2023-01-25 09:51:37 -0500

    fix tag

--- src/i18n/en-us.js
+++ src/i18n/en-us.js
@@ -221,7 +221,7 @@ export default {
       },
     },
     fridgeSites: {
-      header:  'Community Refrigerators',
+      header:  'Community refrigerators',
       eligibility: 'Anyone can visit a Community Refrigerator to pick up food.',
       pickupDetails: 'Community Refrigerators are open 24 hours a day, 7 days a week and anyone can take the available food at any time. Community Refrigerators are run by community-based organizations and volunteers. \
       Availability of food will vary based on donations. \
