4e2efb216f8b198355a36501f68b95218fb17eb7
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-09-11 11:58:53 -0400

    uses new 3_0 endpoint

--- src/data-sources/covid-free-meal-sites.js
+++ src/data-sources/covid-free-meal-sites.js
@@ -4,7 +4,7 @@ export default {
   dependent: 'none',
   resettable: false,
   // url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/COVID19_FreeMealSites_2_0_FOR_TESTING/FeatureServer/0/query',
-  url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/COVID19_FreeMealSites_2_0_PUBLICVIEW/FeatureServer/0/query',
+  url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/COVID19_FreeMealSites_3_0_PUBLICVIEW/FeatureServer/0/query',
   options: {
     params: {
       where: '1=1',
