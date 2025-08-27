bb42aeb6210bed2063924d41229e5783e53244b7
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-08-27 13:45:25 -0400

    uses new translations

---
+++ src/i18n/vi.js
@@ -0,0 +1,265 @@
+export default{
+  "app": {
+    "title": "Công cụ tìm thức ăn và bữa ăn",
+    "subtitle": "Nhận thực phẩm, dịch vụ dinh dưỡng và lợi ích công cộng miễn phí",
+    "bannerAlert": "Nhiều trang web đã đóng cửa ngày hôm nay. Kiểm tra chi tiết trang web cụ thể để biết thêm thông tin.",
+    "noResults": "Chúng tôi rất tiếc, không có kết quả cho tìm kiếm đó. Điều chỉnh các bộ lọc bạn đã chọn và thử lại."
+  },
+  "categoryType": {
+    "category": "Thể loại",
+    "generalMealSite": "Trang web bữa ăn chung",
+    "foodSite": "Trang web thực phẩm",
+    "studentMealSite": "Trang web bữa ăn dành cho sinh viên",
+    "olderAdultMealSite": "Trang web bữa ăn dành cho người lớn tuổi",
+    "communityRefrigerator": "Tủ lạnh cộng đồng",
+    "publicBenefits": "Hỗ trợ thực phẩm và lợi ích"
+  },
+  "eligibility": "Tính đủ điều kiện",
+  "pickupDetails": "Chi tiết",
+  "details": "Chi tiết",
+  "beforeYouGo": "Trước khi bạn đi",
+  "change": "Thay đổi lịch trình",
+  "closure": "Đóng cửa tạm thời",
+  "newSites": "Trang web bữa ăn mùa hè mở cửa vào Thứ Hai 6/16/2025",
+  "holidayClosure": "Trang web này hiện đang tạm thời đóng cửa ",
+  "futureHolidayClosure": "Trang web này sẽ tạm thời đóng cửa vì ",
+  "holidayClosureAllSites_juneteenth": "Ngày mười hai tháng 6 - Giờ có thể thay đổi cho kỳ nghỉ vào ngày 6/19. Liên hệ với trang web trước khi bạn đi.",
+  "holidayClosureAllSites_thanksgiving": "Lễ Tạ ơn - Giờ có thể thay đổi cho kỳ nghỉ cuối tuần. Liên hệ với trang web trước khi bạn đi.",
+  "holidayClosureAllSites_weekend": "Giờ có thể thay đổi trong kỳ nghỉ cuối tuần. Liên hệ với trang web trước khi bạn đi.",
+  "holidayClosureAllSites": "Giờ có thể thay đổi trong kỳ nghỉ cuối tuần. Liên hệ với trang web trước khi bạn đi.",
+  "winterHolidayClosureAllSites": "Ngày lễ Giáng sinh và năm mới - Giờ có thể thay đổi trong kỳ nghỉ đông. Liên hệ với trang web trước khi bạn đi.",
+  "futureHolidayClosureAllSites": "Giờ có thể thay đổi trong kỳ nghỉ cuối tuần. Liên hệ với trang web trước khi bạn đi.",
+  "weatherClosure": "Trang web này hiện đang tạm thời đóng cửa do thời tiết.",
+  "temporaryClosure": "Trang web này hiện đang tạm thời đóng cửa.",
+  "schedule": "Lịch trình",
+  "nonPerish": "chỉ các mặt hàng không dễ hỏng",
+  "freshOnly": "chỉ sản phẩm tươi sống",
+  "breakfastLunch": "Ăn sáng và bữa trưa",
+  "tenMeals": "Các trang web cung cấp 7 bữa sáng và 7 bữa ăn trưa và trái cây tươi mỗi tuần.",
+  "oneMeal": "1 bữa ăn cho mỗi cư dân",
+  "driveThrough": {
+    "dt": "Drive-thru",
+    "wu": "Đi bộ lên",
+    "both": "Lái xe và đi bộ"
+  },
+  "daysAndTimesVaryBySite": "Ngày và thời gian khác nhau tùy theo trang web.",
+  "sections": {
+    "foodSites": {
+      "header": "Các trang web thực phẩm",
+      "eligibility": "Bất kỳ cư dân nào cũng đủ điều kiện. Không yêu cầu ID hoặc bằng chứng thu nhập.",
+      "eligibilityZipCodeException": "Không yêu cầu ID hoặc bằng chứng thu nhập. Cư dân phải sống trong các mã zip sau để đủ điều kiện: ",
+      "pickupDetails": {
+        "p1": "Các trang web thực phẩm cung cấp thực phẩm bổ sung và hàng tạp hóa.",
+        "p3": "Tùy thuộc vào địa điểm, cư dân có thể nhận được:",
+        "li1": "Hộp thực phẩm đóng gói sẵn.",
+        "li2": "Sự lựa chọn thực phẩm của họ từ một tủ đựng thức ăn.",
+        "li3": "Họ lựa chọn thực phẩm từ tủ lạnh cộng đồng."
+      },
+      "subsections": {
+        "PHILABUNDANCE": {
+          "name": "Sự phong phú"
+        },
+        "PHILABUNDANCE/SHARE FOOD PROGRAM": {
+          "name": "Chương trình từ hiểm/Chia sẻ thực phẩm"
+        },
+        "SHARE FOOD PROGRAM": {
+          "name": "Chia sẻ chương trình thực phẩm"
+        },
+        "Nutritional Development Services": {
+          "name": "Dịch vụ phát triển dinh dưỡng"
+        },
+        "Small Things Philly": {
+          "name": "Những điều nhỏ nhặt Philly"
+        },
+        "Community Refrigerators": {
+          "name": "Tủ lạnh cộng đồng",
+          "eligibility": "Bất cứ ai cũng có thể ghé thăm Tủ lạnh Cộng đồng để lấy thức ăn.",
+          "pickupDetails": "Tủ lạnh cộng đồng mở cửa 24 giờ một ngày, 7 ngày một tuần và bất cứ ai cũng có thể lấy thức ăn có sẵn bất cứ lúc nào. Tủ lạnh cộng đồng được điều hành bởi các tổ chức và tình nguyện viên dựa trên cộng đồng. Sự sẵn có của thực phẩm sẽ thay đổi dựa trên sự đóng góp. Truy cập mỗi trang web Tủ lạnh Cộng đồng để biết thêm chi tiết."
+        },
+        "General Food Site": {
+          "name": "Trang web thực phẩm chung"
+        }
+      }
+    },
+    "generalMealSites": {
+      "header": "Trang web bữa ăn chung",
+      "eligibility": "Bất kỳ cư dân nào cũng đủ điều kiện. Không yêu cầu ID hoặc bằng chứng thu nhập.",
+      "pickupDetails": {
+        "p1": "Các trang web bữa ăn nói chung cung cấp các bữa ăn sẵn."
+      },
+      "subsections": {
+        "PHILABUNDANCE meal": {
+          "name": "Sự phong phú"
+        },
+        "Office of Homeless Services": {
+          "name": "Văn phòng Dịch vụ Vô gia cư"
+        },
+        "STP": {
+          "name": "Những điều nhỏ nhặt Philly"
+        }
+      }
+    },
+    "olderAdultMealSites": {
+      "header": "Trang web bữa ăn dành cho người lớn tuổi",
+      "eligibility": {
+        "p1": "Độ tuổi đủ điều kiện thay đổi tùy theo trang web.",
+        "p2": "Vợ chồng của người lớn đủ điều kiện cũng có thể nhận được bữa ăn tại các địa điểm này, bất kể tuổi tác của họ."
+      },
+      "pickupDetails": {
+        "p1": "Các trang web bữa ăn dành cho người lớn tuổi cung cấp các bữa ăn sẵn.",
+        "p4": "Ưu tiên cư dân đặt trước bữa ăn trước ít nhất một tuần."
+      },
+      "subsections": {
+        "PCA": {
+          "name": "Các trang web được hỗ trợ PCA",
+          "eligibility": {
+            "li1": "Người lớn từ 60 tuổi trở lên đủ điều kiện tại các địa điểm được hỗ trợ bởi Tập đoàn Lão hóa Philadelphia (PCA).",
+            "p2": "Để tìm hiểu về các dịch vụ khác hoặc nhận trợ giúp bằng ngôn ngữ khác, hãy gọi đường dây trợ giúp của PCA theo số <b><a class='underlined' href='tel:+12157659040'>(215)</a> 765-9040."
+          },
+          "pickupDetails": {
+            "p1": "Các trang web bữa ăn dành cho người lớn tuổi cung cấp các bữa ăn sẵn.",
+            "p3": "Một cư dân có thể ăn tối đa năm bữa mỗi tuần tại địa điểm này. Các bữa ăn được phục vụ trong khuôn viên.",
+            "p4": "Ưu tiên cư dân đặt trước bữa ăn trước ít nhất một tuần. Gọi cho trang web để tìm hiểu thêm và đặt chỗ."
+          }
+        },
+        "specialPCA": {
+          "eligibility": {
+            "li1": "Người lớn từ 60 tuổi trở lên đủ điều kiện tại các địa điểm được hỗ trợ bởi Tập đoàn Lão hóa Philadelphia (PCA).",
+            "p2": "Để tìm hiểu về các dịch vụ khác hoặc nhận trợ giúp bằng ngôn ngữ khác, hãy gọi đường dây trợ giúp của PCA theo số <b><a class='underlined' href='tel:+12157659040'>(215)</a> 765-9040."
+          },
+          "pickupDetails": {
+            "p1": "Các trang web bữa ăn dành cho người lớn tuổi cung cấp các bữa ăn sẵn.",
+            "p3": "Một cư dân có thể ăn tối đa bảy bữa mỗi tuần tại địa điểm này. Điều này bao gồm sự kết hợp giữa các bữa ăn tại chỗ và bữa ăn mang đi.",
+            "p4": "Ưu tiên cư dân đặt trước bữa ăn trước ít nhất một tuần. Gọi cho trang web để tìm hiểu thêm và đặt chỗ."
+          }
+        },
+        "PPR_Senior": {
+          "name": "Trung tâm dành cho người lớn tuổi ở Philadelphia Parks & Recreation",
+          "eligibility": {
+            "li1": "Người lớn từ 55 tuổi trở lên đủ điều kiện tại các trung tâm dành cho người lớn tuổi ở Parks & Recreation.",
+            "p2": "Để tìm hiểu về các dịch vụ khác hoặc nhận trợ giúp bằng ngôn ngữ khác, hãy gọi đường dây trợ giúp của PCA theo số <b><a class='underlined' href='tel:+12157659040'>(215)</a> 765-9040."
+          },
+          "pickupDetails": {
+            "p1": "Các trang web bữa ăn dành cho người lớn tuổi cung cấp các bữa ăn sẵn.",
+            "p3": "Cư dân có thể nhận 5 bữa mỗi tuần tại hầu hết các địa điểm."
+          }
+        }
+      }
+    },
+    "studentMealSites": {
+      "header": "Trang web bữa ăn của sinh viên",
+      "offSeason": {
+        "p1": "Tất cả học sinh ở Học khu Philadelphia đều nhận được bữa sáng và bữa trưa miễn phí tại trường. Để tìm hiểu thêm, hãy xem trang dịch vụ ăn uống của <a target='_blank' href='https://www.philasd.org/foodservices/'>quận.</a>",
+        "p2": "Những người trẻ tuổi cũng có thể nhận đồ ăn nhẹ hoặc bữa ăn tại các chương trình <a target='_blank' href='https://www.phila.gov/ost/program-locator/#/'>Out of School Time.</a>"
+      },
+      "eligibility": "Trẻ em và thanh thiếu niên từ 18 tuổi trở xuống đủ điều kiện. Không yêu cầu ID.",
+      "pickupDetails": {
+        "p1": "Các trang web bữa ăn dành cho sinh viên cung cấp các bữa ăn sẵn."
+      },
+      "otherResources": {
+        "p1": "Để tìm thêm bữa ăn mùa hè cho giới trẻ:",
+        "li1": "Gọi cho Đường dây nóng Bữa ăn Mùa hè theo <a href='tel:215-770-4659'>số (215) 770-4659</a>.",
+        "li2": "Nhắn tin “THỰC PHẨM” hoặc “COMIDA” đến <a href='tel:914-342-7744'>9</a> 14-342-7744.",
+        "li3": "Truy cập <a target='_blank' href='http://www.phillysummermeals.org'>www.ph</a> illysummermeals.org.",
+        "li4": "Truy cập <a target='_blank' href='https://www.fns.usda.gov/summer/sitefinder'>www.fn</a> s.usda.gov/summer/sitefinder."
+      },
+      "subsections": {
+        "PHA": {
+          "name": "Trung tâm cộng đồng Cơ quan Nhà ở Philadelphia (PHA)",
+          "pickupDetails": "Các bữa ăn phải được ăn tại chỗ. Để biết thêm thông tin, hãy truy cập trang web."
+        },
+        "playstreets": {
+          "name": "Playstreet",
+          "pickupDetails": "Playstreet mở cửa từ thứ Hai đến thứ Sáu từ 10:00 sáng đến 4:00 chiều Đường phố đóng cửa cho giao thông ô tô. Thời gian bữa ăn khác nhau tùy theo địa điểm. Để biết thêm thông tin, hãy truy cập Playstreet. <br>Playstreets phục vụ bữa trưa và bữa ăn nhẹ."
+        },
+        "NDS": {
+          "name": "Dịch vụ phát triển dinh dưỡng",
+          "pickupDetails": "Các bữa ăn phải được ăn tại chỗ. Liên hệ với NDS để biết thêm chi tiết."
+        },
+        "Recreation Center": {
+          "name": "Trung tâm giải trí",
+          "pickupDetails": "Thứ Hai đến Thứ Sáu, từ 9:00 sáng đến 3:00 chiều Thời gian bữa ăn khác nhau tùy theo địa điểm. Các bữa ăn phải được ăn tại chỗ. Để biết thêm thông tin, hãy gọi hoặc truy cập Trung tâm Giải trí."
+        },
+        "Caring for Friends": {
+          "name": "Chăm sóc bạn bè",
+          "pickupDetails": "Các bữa ăn phải được ăn tại chỗ. Liên hệ với trang web để biết thêm chi tiết."
+        },
+        "Philabundance Summer Meal Sites": {
+          "name": "Trang web bữa ăn mùa hè Phil",
+          "pickupDetails": "Liên hệ với trang web để biết thêm chi tiết."
+        },
+        "Other Summer Meal Sites": {
+          "name": "Các trang web bữa ăn mùa hè khác",
+          "pickupDetails": "Thời gian bữa ăn khác nhau tùy theo địa điểm. Các bữa ăn phải được ăn tại chỗ. Để biết thêm thông tin, hãy truy cập trang web bữa ăn mùa hè."
+        },
+        "JSJ Food Bank": {
+          "name": "Trang web bữa ăn mùa hè của Ngân hàng Thực phẩm JSJ",
+          "pickupDetails": "Các bữa ăn phải được ăn tại chỗ. Liên hệ với trang web để biết thêm chi tiết."
+        },
+        "ITAVTFOC Summer Meal Sites": {
+          "name": "Phải mất một ngôi làng để nuôi một đứa trẻ các địa điểm ăn uống mùa hè",
+          "pickupDetails": "Các bữa ăn phải được ăn tại chỗ. Liên hệ với trang web để biết thêm chi tiết."
+        },
+        "Feed Philly Now Summer Meal Sites": {
+          "name": "Trang web bữa ăn mùa hè cho Philly Now",
+          "pickupDetails": "Các bữa ăn phải được ăn tại chỗ. Liên hệ với trang web để biết thêm chi tiết."
+        }
+      }
+    },
+    "publicBenefits": {
+      "header": "Hỗ trợ thực phẩm và lợi ích",
+      "eligibility": "Tính đủ điều kiện khác nhau tùy theo trang web.",
+      "pickupDetails": {
+        "p1": "Tùy thuộc vào địa điểm, cư dân có thể:",
+        "li1": "Tìm hiểu về và đăng ký các quyền lợi công cộng.",
+        "li2": "Nhận dịch vụ dinh dưỡng và hỗ trợ.",
+        "li3": "Nhận giới thiệu đến chăm sóc sức khỏe hoặc dịch vụ xã hội.",
+        "p2": "Ngày, thời gian và dịch vụ khác nhau tùy theo trang web."
+      },
+      "subsections": {
+        "WIC": {
+          "name": "WIC",
+          "eligibility": "WIC phục vụ những người mang thai, những người gần đây đã có con và trẻ em dưới năm tuổi, bao gồm cả con nuôi. Để nhận được WIC, bạn phải đáp ứng <a href='https://pawic.com/IncomeGuidelines.aspx' target='_blank'>các nguyên tắc thu nhập</a>.",
+          "pickupDetails": "WIC cung cấp các dịch vụ dinh dưỡng, hỗ trợ nuôi con bằng sữa mẹ và giới thiệu cho các dịch vụ chăm sóc sức khỏe và xã hội. Thông qua WIC, bạn cũng có thể nhận được lợi ích để giúp bạn mua thực phẩm lành mạnh. Để bắt đầu, hãy gọi cho văn phòng WIC mà bạn dự định đến thăm."
+        },
+        "BenePhilly": {
+          "name": "BenePhilly",
+          "eligibility": "Bất kỳ cư dân nào cũng đủ điều kiện đến trung tâm BenePhilly.",
+          "pickupDetails": "Các trung tâm BenePhilly cung cấp hỗ trợ trực tiếp miễn phí để giúp cư dân đăng ký nhận các quyền lợi công cộng. Những chương trình này có thể giúp bạn trả tiền cho hàng tạp hóa, thuốc men, bảo hiểm y tế và các tiện ích. Ngày và giờ khác nhau tùy theo trang web. <br><br>Trước khi bạn đi, hãy gọi cho trung tâm BenePhilly mà bạn dự định ghé thăm."
+        }
+      }
+    },
+    "moreResources": {
+      "header": "Thêm tài nguyên",
+      "p1": "Bạn muốn tìm hiểu thêm? Khám phá các tùy chọn khác để <a href='https://www.phila.gov/services/mental-physical-health/find-free-food-or-meals/' target='_blank'>nhận thức ăn miễn phí và hỗ trợ bổ sung</a>. Nếu bạn cần trợ giúp <a href='https://www.phila.gov/services/payments-assistance-taxes/financial-services-for-residents/get-free-help-applying-for-public-benefits/' target='_blank'>để xin trợ cấp công cộng, hãy</a> gọi Đường dây trợ giúp BenePhilly theo số <a href='tel:8009945537'>(800) 994-5537</a>."
+    }
+  },
+  "time": {
+    "category": "Thời gian trong ngày",
+    "morning": "Buổi sáng (trước buổi trưa)",
+    "afternoon": "Chiều (trưa - 5:00 chiều)",
+    "evening": "Buổi tối (sau 5:00 chiều)"
+  },
+  "checkSite": "Chi tiết trang web có thể thay đổi. Trước khi bạn đi, hãy liên hệ với trang web để xác nhận giờ và dịch vụ của họ.",
+  "otherResources": "Tài nguyên khác",
+  "exceptions": {
+    "Open on second and fourth Tuesday of the month": "Mở cửa vào thứ Ba thứ hai và thứ tư trong tháng",
+    "Open on first and third Saturday of the month": "Mở cửa vào thứ Bảy đầu tiên và thứ ba trong tháng",
+    "Open on second and fourth Monday of the month": "Mở cửa vào thứ Hai thứ hai và thứ tư trong tháng",
+    "Open on first and third Wednesday of the month": "Mở cửa vào thứ Tư đầu tiên và thứ ba trong tháng",
+    "Open on first Monday of the month": "Mở cửa vào thứ Hai đầu tiên của tháng",
+    "Open on first Tuesday of the month": "Mở cửa vào thứ Ba đầu tiên của tháng",
+    "Open on third Thursday of the month": "Mở cửa vào thứ Năm thứ ba trong tháng",
+    "Open on third and fourth Saturday of the month": "Mở cửa vào thứ bảy thứ ba và thứ tư của tháng",
+    "Open on first and third Mondays of the month": "Mở cửa vào thứ Hai đầu tiên và thứ ba trong tháng",
+    "Open on the last Thursday of the month": "Mở cửa vào thứ Năm cuối cùng của tháng",
+    "Open on first and third Friday of the month": "Mở cửa vào thứ Sáu đầu tiên và thứ ba của tháng",
+    "Open on second and fourth Saturday of the month": "Mở cửa vào thứ bảy thứ hai và thứ tư trong tháng",
+    "Open on first Friday of the month": "Mở cửa vào thứ Sáu đầu tiên của tháng",
+    "Open on first and third Monday of the month": "Mở cửa vào thứ Hai đầu tiên và thứ ba của tháng",
+    "Open on third Saturday of the month": "Mở cửa vào thứ bảy thứ ba trong tháng",
+    "Open on second and fourth Wednesday of the month": "Mở cửa vào thứ Tư thứ hai và thứ tư trong tháng",
+    "Open on second and fourth Saturdays of the month": "Mở cửa vào thứ bảy thứ hai và thứ tư trong tháng",
+    "Open for Breakfast 7:00-8:30, Lunch 12:00-1:30, and Dinner 4:00-5:30": "Mở cửa phục vụ bữa sáng 7:00-8:30, Bữa trưa 12:00-1:30, và Bữa tối 4:00-5:30"
+  }
+}
