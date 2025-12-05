8f625ced6e97dc2d948a8e61fe2151f738e29fc0
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-12-05 13:14:51 -0500

    deletes commented out code

--- src/components/NeighborhoodCommunityActionCenterCard.vue
+++ src/components/NeighborhoodCommunityActionCenterCard.vue
@@ -75,50 +75,6 @@ const parseException = (exception, index, i18nLocale) => {
       v-html="$t('sections.'+section+'.subsections.'+subsection+'.pickupDetails')"
     />
 
-    <!-- <vue-good-table
-      :columns="pickupDetails.columns"
-      :rows="pickupDetails.rows"
-      :sort-options="{ enabled: false }"
-      style-class="table"
-    >
-      <template #table-column="props">
-        <span
-          v-if="props.column.label =='Days'"
-          class="table-header-text"
-        >
-          {{ $t(props.column.i18nLabel) }}
-        </span>
-        <span
-          v-if="props.column.label =='Schedule'"
-          class="table-header-text"
-        >
-          {{ $t(props.column.i18nLabel) }}
-        </span>
-      </template>
-
-      <template #table-row="props">
-        <span
-          v-if="props.column.field == 'label'"
-          class="table-text"
-        >
-          {{ $t(props.row.days) }}
-        </span>
-        <div
-          v-if="props.column.field == 'value'"
-          class="table-text"
-        >
-          {{ props.row.schedule }}
-        </div>
-      </template>
-    </vue-good-table>
-    <div class="exceptions-holder">
-      <div
-        v-for="(exception, index) of exceptionsList"
-        :key="index"
-      >
-        {{ parseException(exception, index+1, i18nLocale) }}
-      </div>
-    </div> -->
   </section>
 </template>
 
